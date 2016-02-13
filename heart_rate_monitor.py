1fe7f5cd0600193442f350a2c595d98d8355e488
jonkeller <kellejm2+git@gmail.com>
2016-02-13 15:57:46 -0500

    Add DB code

--- heart_rate_monitor.py
+++ heart_rate_monitor.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+import MySQLdb
  
 SLEEP_TIME = 0.001
  
@@ -10,10 +11,16 @@ GPIO.setmode(GPIO.BCM)
 HRM_PIN = 23
 GPIO.setup(HRM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 value = 0
+
+db = MySQLdb.connect("localhost", "hrm", "heartbeat", "HeartRate")
+curs = db.cursor()
+
  
 while True:
     newValue = GPIO.input(HRM_PIN)
     if newValue and not value:
         print datetime.datetime.now().time().isoformat(), "\tBeat"
+        curs.execute("INSERT INTO Beats VALUES (NOW())")
+        db.commit()
     value = newValue
     time.sleep(SLEEP_TIME)
